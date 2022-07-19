d58259cb7acfc59d1c7b6835297d8822a057c291
ABHIRAM REDDY <anukoolaabhiram@outlook.com>
2022-07-19 17:30:19 +0530

    secure added on cookie attribute

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -20,6 +20,7 @@ const sendToken = (user: UserObj, statusCode: number, res: Response) => {
     expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: 'none',
+    secure: true,
   };
 
   res.status(statusCode).cookie('token', token, options).json({
